b6641912e6fcc323ace3de55fbfaf21961dfcbf2
Diasayu <diasayusavitri07@outlook.com>
2020-06-15 11:54:44 +0700

    Berhasil menggunakan Table

--- src/Component/Class/BootstrapComp.jsx
+++ src/Component/Class/BootstrapComp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Alert, Button } from 'reactstrap';
+import { Container, Row, Col, Table } from 'reactstrap';
 
 class Bootstrap extends Component {
     constructor(props) {
@@ -14,13 +14,38 @@ class Bootstrap extends Component {
         return (
 
         <div>
-             <Button color="primary">primary</Button>{' '}
-             <Button color="secondary">secondary</Button>{' '}
-             <Button color="success">success</Button>{' '}
-             <Button color="info">info</Button>{' '}
-             <Button color="warning">warning</Button>{' '}
-             <Button color="danger">danger</Button>{' '}
-             <Button color="link">link</Button>
+            <Container>
+                <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+            </Container>
         </div>
         
         )
