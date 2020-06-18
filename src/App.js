cf1650a1c9d07c07f18b56d90c24b4e37d094517
Diasayu <diasayu7100@gmail.com>
2020-06-17 17:41:02 -0700

    Berhasil menyelesaikan video 27 dan 28

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import BootstrapComp from './Component/Class/BootstrapComp';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+//import BootstrapComp from './Component/Class/BootstrapComp';
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
+import DetailComp from './Component/Fungsional/DetailComp';
 
 //import logo from './logo.svg';
 //import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
 //import './App.css';~   
 
-function App() { 
+const App = () => {
   return (
-    <div className="App ">
-      <BootstrapComp/>
-    </div>
-  );      
+    <BrowserRouter>
+      <NavbarComp />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/detail/:id" component={DetailComp} />
+
+      </Switch>
+    </BrowserRouter>
+
+
+  );
 }
 
 export default App;
