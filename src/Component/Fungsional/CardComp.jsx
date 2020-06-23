50969e466487dbcc7d8b238e785aa59fe18c34fb
Diasayu <diasayu7100@gmail.com>
2020-06-23 10:55:50 -0700

    Berhasil menyelesaikan video 29

--- src/Component/Fungsional/CardComp.jsx
+++ src/Component/Fungsional/CardComp.jsx
@@ -9,7 +9,7 @@ function CardComp(props) {
     return (
         <div>
             <Card>
-                <CardImg top width="100%" src="https://placeimg.com/640/480/animals" alt="Card image cap" />
+                <CardImg top width="100%" src="https://placeimg.com/640/480/tech" alt="Card image cap" />
                 <CardBody>
                     <CardTitle><h3>{props.judul}</h3></CardTitle>
                     <CardSubtitle>{props.tanggal}</CardSubtitle>
@@ -20,7 +20,7 @@ function CardComp(props) {
                                 pathname : '/detail/${props.id}',
                                 state : {
                                     judul: props.judul,
-                                    tanggal: props.tanggal,
+                                    tanggal: props.tanggal
                                 }
                             }
                         }>Detail</Link>
