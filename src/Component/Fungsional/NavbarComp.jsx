e9fb9cb90c3c9c8a87a5091da3d6652f77c4190e
Diasayu <diasayu7100@gmail.com>
2020-06-23 18:44:22 -0700

    Selesai video 30 tapi masih error tidak mau muncul isi datanya

--- src/Component/Fungsional/NavbarComp.jsx
+++ src/Component/Fungsional/NavbarComp.jsx
@@ -28,6 +28,9 @@ const NavbarComp = (props) => {
                         <NavItem>
                             <NavLink href="/about">About</NavLink>
                         </NavItem>
+                        <NavItem>
+                            <NavLink href="/mahasiswa">Mahasiswa</NavLink>
+                        </NavItem>
                     </Nav>
                     <NavbarText>Simple Text</NavbarText>
                 </Collapse>
