716b10b92d3b1944aede2f9e0d1dd4877899319e
Diasayu <diasayu7100@gmail.com>
2020-07-08 12:53:14 -0700

    Berhasil selesai video 35 React Hooks useEffects

--- src/Component/Hooks/Functional/HooksUseEffects.jsx
+++ src/Component/Hooks/Functional/HooksUseEffects.jsx
@@ -1,20 +1,48 @@
-import React, {useEffect, useState} from 'react'
-import { Container } from 'reactstrap'
+import React, { useEffect, useState } from 'react'
+import { Container, Table, Button } from 'reactstrap'
+import axios from 'axios'
+
+const api = 'http://localhost:3001'
 
 function HooksUseEffects() {
 
-    const[namaLengkap, setNamaLengkap] = useState({nama:'Dias', marga:'Ayu'})
+    const [mahasiswa, setMahasiswa] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("Memanggil Use Effects")
-        setNamaLengkap({nama : 'Munif', marga:'Abdillah'})
-    },[])
+        axios.get(api + '/tampil').then(res => {
+            setMahasiswa(res.data.values)
+        })
+    }, [])
 
     return (
         <Container>
-            <h3>Profil User</h3>
-            <h4>Nama : {namaLengkap.nama}</h4>
-            <h4>Marga : {namaLengkap.marga}</h4>
+            <h2>Data Mahasiswa</h2>
+            <hr />
+            <Table className="table=bordered">
+                <thead>
+                    <tr>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Jurusan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mahasiswa.map(mahasiswa =>
+                        <tr key={mahasiswa.id_mahasiswa}>
+                            <td>{mahasiswa.nim}</td>
+                            <td>{mahasiswa.nama}</td>
+                            <td>{mahasiswa.jurusan}</td>
+                            <td>
+                                <Button> Edit</Button>
+                                <span> </span>
+                                <Button color="danger">Hapus</Button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
         </Container>
     )
 }
