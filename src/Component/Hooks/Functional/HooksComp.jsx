e64236c2f8561d2afb71873714a8fb9707609e56
Diasayu <diasayu7100@gmail.com>
2020-07-08 11:38:29 -0700

    Berhasil selesai video 34 React Hooks UseState

--- src/Component/Hooks/Functional/HooksComp.jsx
+++ src/Component/Hooks/Functional/HooksComp.jsx
@@ -1,12 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import TampilComp from './TampilComp';
 
 const HooksComp = () => {
-const [jumlah, setJumlah] = useState(0)
+    const [jumlah, tambahJumlah] = useState(0)
+
+    //login
+    const [dataLogin, setDataLogin] = useState({ username: 'dias', token: '123abcd', isLogin: true })
+
+    //cek kondisi sudah login / belum
+
+    let tampil;
+
+    if (dataLogin.isLogin) {
+        tampil = <TampilComp
+            username={dataLogin.username}
+            fungsi={tambahJumlah.bind(this)}
+            jumlah = {jumlah} />
+    } else {
+        tampil = <TampilComp username="Maaf anda belum login" disabled= {true}/>
+    }
+
 
     return (
         <div>
-            <h1>{jumlah}</h1>
-            <button onClick={()=>setJumlah(jumlah+1)}>Tambah</button>
+            {tampil}
         </div>
     )
 }
